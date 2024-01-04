263664165ee795d0856af1339c731893d3ac345a
BukkaRamesh <bukkaramesh.b@gmail.com>
2024-01-04 17:50:33 -0500

    initial commit

---
+++ src/main/java/com/walmart/util/Sample.java
@@ -0,0 +1,47 @@
+package com.walmart.util;
+
+public class Sample {
+	
+	
+	
+	/*
+	 * 
+	 * 
+	 * 1. we need our reporsiory  -- walmart
+	 * 2. you need to clone the code -- the ways to clone the code is via eclipse or use git bash shell 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 *   Git commands to perform operations ***
+	 *   
+	 *   git clone https://
+	 *   cd Walmart/
+	 *   git branch -- what's your current branch is 
+	 *   git checkout -b feature/xyz   -- create new branch
+	 *   git add filepath  -- it will add the changes to git 
+	 *   git commit -m "replace me"  -- to cmmit the code
+	 *   git push -u origin feature/ramesh
+	 *   git pull -- will fetch all the latest code from repo
+	 *   
+	 *   
+	 *   
+	 * 
+	 *   
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+	
+
+}
